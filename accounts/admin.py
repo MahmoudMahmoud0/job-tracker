ea19aa2429f2d08cebea1d423a72cc39c6c9e444
Mahmoud Mahmoud <mahmoud.mhd1@outlook.com>
2026-07-06 23:14:12 +0300

    added user model to admin site with customizations

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .forms import User
 
-# Register your models here.
+admin.site.site_header = "Jon Tracker Administration"
+admin.site.site_title = "Job Tracker Admin"
+admin.site.index_title = "Welcome to Job Tracker"
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "full_name",
+        "email",
+        "is_email_verified",
+        "is_active",
+        "is_staff",
+    ]
+
+    list_filter = ["is_email_verified", "is_staff", "is_active"]
+
+    search_fields = ["email", "first_name", "last_name"]
+
+    @admin.display(description="Name", ordering="first_name")
+    def full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}".strip()
+
+admin.site.register(User, UserAdmin)
