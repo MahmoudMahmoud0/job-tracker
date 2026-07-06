280d47e489656f4f7e6a525e8f13b4b0c17328ee
Mahmoud Mahmoud <mahmoud.mhd1@outlook.com>
2026-07-06 23:19:46 +0300

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
+from django.contrib import admin
 
 class User(AbstractUser):
     username = models.CharField(max_length=150, blank=True)
     email = models.EmailField(_("email"), unique=True)
     first_name = models.CharField(_("first name"), max_length=150)
     last_name = models.CharField(_("last name"), max_length=150)
-    is_email_verified = models.BooleanField(default=False)
+    is_email_verified = models.BooleanField(default=False, verbose_name="Email Verification Status",)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
